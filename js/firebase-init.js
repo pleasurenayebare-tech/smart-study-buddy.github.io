82b470f3097d35d024f1f99ea0f8a90abc2684fa
pleasurenayebare-tech <pleasurenayebare@gmail.com>
2026-07-13 21:46:36 -0700

    Improve landing page, dashboard, and Firebase config

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,14 +1,18 @@
 (function () {
   "use strict";
 
+  // ########################################################
+  // # Firebase initialization configuration
+  // # Only authenticated emails and verified usernames should be allowed in app logic.
+  // ########################################################
   const firebaseConfig = {
-    apiKey: "AIzaSyBR8HLqUJ1sQRHGH7TWmwwgFBZB1WyWnlU",
-    authDomain: "smart-study-buddy-5ba73.firebaseapp.com",
-    projectId: "smart-study-buddy-5ba73",
-    storageBucket: "smart-study-buddy-5ba73.firebasestorage.app",
-    messagingSenderId: "214153993094",
-    appId: "1:214153993094:web:d73b3c2f9c002e7e362a07",
-    measurementId: "G-ZHN9J1M1EB"
+    apiKey: "AIzaSyDkPmJVJzfmF7hPrb7wAi5BSL38430Wn04",
+    authDomain: "smart-study-buddy-16111.firebaseapp.com",
+    projectId: "smart-study-buddy-16111",
+    storageBucket: "smart-study-buddy-16111.firebasestorage.app",
+    messagingSenderId: "318367609801",
+    appId: "1:318367609801:web:c4498bf48c98c01bbcee18",
+    measurementId: "G-8S6D10GN5J"
   };
 
   const statusElement = document.getElementById("firebase-status");
